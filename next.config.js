fcfc1123b7aa2aee1f67fd139935568f25013f23
shar27 <shariq.ahmed731@gmail.com>
2023-02-05 16:34:17 +0100

    home

---
+++ next.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: 'https://homedecoraccessories.co.uk/',
+        permanent: true,
+      },
+    ]
+  },
+}

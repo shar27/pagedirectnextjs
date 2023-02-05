673fecb1d3e4d1acc51727f899278fd69e0e18f6
shar27 <shariq.ahmed731@gmail.com>
2023-02-05 17:10:37 +0100

    push

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Redirecting to Home Decor Accessories 
+          Redirecting Home Decor Accessories 
         </h1>
 
       </main>
